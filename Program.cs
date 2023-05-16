ce9c77ad224a9bb4231d38ae75f0a52271b33a42
sergei1212331 <90478003+sergei1212331@users.noreply.github.com>
2023-05-16 16:10:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше слово: ");
+        string word1 = Console.ReadLine();
+
+        Console.Write("Введіть друге слово: ");
+        string word2 = Console.ReadLine();
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (char letter in word1)
+        {
+            if (word2.Contains(letter))
+            {
+                result.Append(letter);
+            }
+        }
+
+        Console.WriteLine("Літери, які є в обох словах: " + result);
+        Console.ReadKey();
+    }
+}
